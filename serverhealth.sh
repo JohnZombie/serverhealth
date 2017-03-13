3830958bec8071b89ce8e42b1ac8ff6c9e3bffb9
Your Name <johnzombie1992@gmail.com>
2017-03-13 00:03:23 -0400

    First Commit

--- serverhealth.sh
+++ serverhealth.sh
@@ -1,91 +1,62 @@
 #!/bin/bash
 
+echo '  _________                                   ___ ___                .__   __  .__     
+ /   _____/ ______________  __ ___________   /   |   \   ____ _____  |  |_/  |_|  |__  
+ \_____  \_/ __ \_  __ \  \/ // __ \_  __ \ /    ~    \_/ __ \\__  \ |  |\   __\  |  \ 
+ /        \  ___/|  | \/\   /\  ___/|  | \/ \    Y    /\  ___/ / __ \|  |_|  | |   Y  \
+/_______  /\___  >__|    \_/  \___  >__|     \___|_  /  \___  >____  /____/__| |___|  /
+        \/     \/                 \/               \/       \/     \/               \/ '
 
+## Date:
+echo -e "\e[1;31mDate and Time:\e[0m" 
 date
 
-echo"========================================"
+## OS Version:
+echo -e "\e[1;31mOS Information:\e[0m"
+uname -m -o -v -r -p
 
 
-#The date command is used to show the systems time and date information.
+## Hostname:
+echo -e "\e[1;31mSystem Hostname:\e[0m"
+hostname
 
+## Check connection to Google:
+echo -e "\e[1;31m Internet Connection Status:\e[0m"
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+        echo "Online"
+else
+        echo "Offline"
+fi
 
-uptime
+## Internal IP address
+echo -e "\e[1;31mInternal IP:\e[0m"
+ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
 
+## External IP address:
+echo -e "\e[1;31mExternal IP:\e[0m"
+curl ifconfig.co
 
-echo "========================================"
+## NMAP to scan for open ports:
+echo -e "\e[1;31mOpen Ports on System:\e[0m"
+nmap -p- -T4 127.0.0.1
 
-#The uptime command gives a one line display of the following information.
-
-   # The current time example:(21:54:11)
-    #How long the system has been running example: (up 13 days)
-    #How many users are currently logged on example: (1 user)
-    #The system load averages for the past 1, 5, and 15 minutes example: (0.21, 0.21, 0.12)
-
-
-
-echo "It is connected:"
-
+echo -e "\e[1;31mSystem Uptime/Current Users Logged In:\e[0m"
 w
 
-#The w command provides a quick summary of every user logged on this server along with load activites.
+## Current RAM Usage:
+echo -e "\e[1;31mRam Usage:\e[0m"
+free -g
+
+## Disk usage:
+echo -e "\e[1;31mDisk Usage Info:\e[0m"
+df -h
+
+## Load Average across cores:
+echo -e "\e[1;31m Load Average:\e[0m"
+top -n 1 -b | grep "load average:" | awk '{print $10 $11 $12}'
+
+## Output Results To Text File
 
 
-echo "========================================="
 
-echo "Last Logins:"
-
-last -a |head -3
-
-#last shows displays a list of all users logged in (and out) since /var/log/wtmp file was created. the -a switch Displays the hostname in the last column. Useful in combination with the next flag. 
-
-#The head command reads the first few lines of any text given to it as an input and writes them to standard output (which, by default, is the display screen). The -3 switch just specifys the first 3 lines.
-
-echo "=========================================="
-
-echo "Disk and Memory Usage:"
-
-df -h  
-
-
-echo "=========================================="
-
-egrep --color 'Mem|Cache|Swap' /proc/meminfo
-
-echo "=========================================="
-
-
-cat /var/log/messages 
-
-echo "=========================================="
-
- echo "Utilization and most important processes:"
-    
-top -b |head -3
-   
-echo "=========================================="
-    
-top -b |head -10 |tail -4
-    
-echo "=========================================="
-    
-echo " Info on open TCP ports......"
-    
-nmap -p- -T4 127.0.0.1
-    
-echo "=========================================="
-    
-echo "Current connections:"
-   
- ss -s
-    
-echo "==========================================="
-    
-echo "processes:"
-    
-ps auxf --width=200
-    
-echo "==========================================="
-    
-echo "vmstat info:"
-
-vmstat 1 5
